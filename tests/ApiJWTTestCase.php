9370f1cb331a885cba31e1595e6ea378a21711fb
ywemay <srv023@ywemay.com>
2020-03-09 21:19:50 +0800

    Working on tests

---
+++ tests/ApiJWTTestCase.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Tests;
+
+// use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+
+class ApiJWTTestCase extends WebTestCase//ApiTestCase
+{
+    // This trait provided by HautelookAliceBundle will take care of refreshing the database content to a known state before each test
+    use RefreshDatabaseTrait;
+
+    private $credentials = array(
+      'username' => 'admin',
+      'password' => 'admin'
+    );
+
+    public function getToken($credentials = []): string
+    {
+      if (!$credentials) $credentials = $this->credentials;
+      $response = static::createClient()->request('POST', '/api/auth/login',
+      ['json' => $credentials ]);
+
+      if($response->getStatusCode() == 200) {
+        $arr = $response->toArray();
+        if (!isset($arr['token'])) return FALSE;
+        return $arr['token'];
+      }
+
+      return FALSE;
+    }
+}
--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -1,26 +1,53 @@
 <?php
-// api/tests/UsersTest.php
 
 namespace App\Tests;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Tests\ApiJWTTestCase;
 use App\Entity\User;
-// use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 
-class UsersTest extends ApiTestCase
+class UsersTest extends ApiJWTTestCase
 {
     // This trait provided by HautelookAliceBundle will take care of refreshing the database content to a known state before each test
-    // use RefreshDatabaseTrait;
+    use RefreshDatabaseTrait;
 
-    private function testLogin(): void
+    public function testGetUsersWithNoLogIn(): void
     {
-      dd('Test is runnning...');
-        $response = static::createClient()->request('POST', '/login', ['json' => [
-            'username' => 'admin',
-            'password' => 'admin',
-        ]]);
+      $client = static::createClient();
+      $response = $client->request('GET', '/api/users');
 
-        $this->assertResponseIsSuccessfull();
-        $this->assertArrayHasKey('token', $response->toArray());
+      // Invalid token expected
+      $this->assertSame(401, $response->getStatusCode());
+    }
+
+    public function testAdminListsUsers(): void
+    {
+      $token = $this->getToken();
+      $client = static::createClient([],
+        [
+          //'auth_bearer' => $token,
+          'headers' => [
+            'Authorization' => 'Bearer ' . $token,
+            'accept' => 'application/ld+json',
+            'content-type' => 'application/json'
+          ],
+          'query' => [
+            'token' => $token
+          ]
+        ]
+      );
+      $response = $client->request('GET', '/api/users');
+      $this->assertSame(200, $response->getStatusCode());
+    }
+
+    public function testLogin(): void
+    {
+      $response = static::createClient()->request('POST', '/api/auth/login', ['json' => [
+        'username' => 'admin',
+        'password' => 'admin',
+      ]]);
+
+      $this->assertSame(200, $response->getStatusCode());
+      $this->assertArrayHasKey('token', $response->toArray());
     }
 }
